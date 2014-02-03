0e46243b147f0b371adbc698ce82192918d0d249
Fateyev <fateyev@frsd.ru>
2014-02-03 19:01:20 +0400

    ttt

--- sync.rb
+++ sync.rb
@@ -36,13 +36,13 @@ board = Trello::Board.all.find { |b| b.name == 'asana' }
 
 workspace = Asana::Workspace.all.find { |w| w.name == 'ОУК' }
 
-project = workspace.projects.all.find { |p| p.name == 'trello' }
+project = workspace.projects.find { |p| p.name == 'trello' }
 
 puts "Migrate Trello board \"#{board.name}\" to Asana wokspace \"#{workspace.name}\", project \"#{project.name}\""
 
 
-list = board.lists.all.find { |l| l.name == 'ToDo' }
-list_doing = board.lists.all.find { |l| l.name == 'Doing' }
+list = board.lists.find { |l| l.name == 'ToDo' }
+list_doing = board.lists.find { |l| l.name == 'Doing' }
 
 
 list.cards.reverse.each do |card|
